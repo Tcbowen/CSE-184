c2ebf9e7ac7984260e35ac7aaef87f8747bca03d
Jeffrey Yeung <jeffrey@eduroam-169-233-227-28.ucsc.edu>
2019-12-03 13:04:46 -0800

    harvey complete

--- 184_project/parse_harvey.py
+++ 184_project/parse_harvey.py
@@ -10,33 +10,47 @@ with open('Harvey_files.csv', mode = 'w', newline = '') as csvfile:
 
     seasons = ['FALL', 'SPRING']
     for season in seasons:
-        for j in range(4,6):
-            if season is 'SPRING' or season is 'FALL':
-                loc = 'HARVEY HTML\\' + season + ' ' + str(j) +'.htm'
-                loc = 'HARVEY HTML\\'
-                print(os.listdir(loc))
+        for j in range(4,20):
+            if (season is 'SPRING' and j > 10) or season is 'FALL':
+                if (j == 6 and season is 'FALL') or (j == 14) or ((j == 13 or j == 17)and season is 'SPRING'):
+                    continue
+                loc = 'HARVEY HTML/' + season + ' ' + str(j) +'.htm'
                 url = codecs.open(loc,'r', 'utf-8')
 
 
-                document = BeautifulSoup(url.text, 'html.parser')
+                document = BeautifulSoup(url, 'html.parser')
                 body = document.find('div', {'class': 'pContent CS'})
                 panel_body = body.find('tbody', {'class': 'gbody'})
                 each_class = panel_body.find_all('tr')
 
-                for i in each_class:
-                    each_body = i.find_all('td', {'valign': 'top'})
-                    class_name = each_body[2].text
-                    #lec_or_lab = each_body.find('div', {'class': 'col-xs-6 col-sm-6'}).text.split(' ')[1]
-                    #enrollment_size = enrollment[2].text
-                    #enrolled = enrollment_size.split(' ')
+                t = 0
 
-                    #class_name = i.a.text;
-                    #class_name_num = class_name.split(' ')[1]
-                    if(lec_or_lab != 'LAB:'):
+                for i in each_class:
+
+                    if (t % 4) == 0:
+                        each_body = i.find_all('td', {'valign': 'top'})
+                        class_name = each_body[2].text
+                        class_size_raw = each_body[3].text
+                        num_people_enrolled = class_size_raw.split("/",1)[0]
+                        total_class_size = class_size_raw.split("/",1)[1]
+                        prof_raw = each_body[5]
+                        prof_raw1 = prof_raw.find_all('li')
+
+                        professor = []
+                        for i in prof_raw1:
+                            prof = i.text
+                            sub_professor = prof.split("/",1)[0]
+                            professor.append(sub_professor)
+
+
+
                         class_info = {
-                        'num_people_enrolled' : enrolled[1],
-                        'total_class_size' : enrolled[3],
+                        'num_people_enrolled' : num_people_enrolled,
+                        'total_class_size' : total_class_size ,
                         'class_number' : class_name,
-                        'term' : season + ' 20' +  str(j)
-						}
+                        'term' : season + ' 20' +  str(j),
+                        'professor' : professor
+    					}
                         writer.writerow(class_info)
+
+                    t = t + 1
