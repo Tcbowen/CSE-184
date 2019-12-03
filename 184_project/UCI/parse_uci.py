04c09e4e2977370c2f80f8369329b734cf702bab
Terry <terryguan1@gmail.com>
2019-12-02 20:05:23 -0800

    Added professors to some schools

--- 184_project/UCI/parse_uci.py
+++ 184_project/UCI/parse_uci.py
@@ -6,7 +6,7 @@ import csv
 
 
 with open('UCI files.csv', mode = 'w', newline = '') as csvfile:
-    writer = csv.DictWriter(csvfile, fieldnames = ['num_people_enrolled', 'total_class_size', 'class_number', 'term'])
+    writer = csv.DictWriter(csvfile, fieldnames = ['num_people_enrolled', 'total_class_size', 'class_number', 'term','professor'])
     writer.writeheader()
 
     url = 'http://www.reg.uci.edu/soc/archives_quarterly.html'
@@ -21,6 +21,7 @@ with open('UCI files.csv', mode = 'w', newline = '') as csvfile:
             term_year = term.text
             term_link = term.get('href')
             term_page = 'http://www.reg.uci.edu/soc/' + term_link
+            #print(term_page)
             term_page_parser = BeautifulSoup(get(term_page).text, 'lxml')
             term_table = term_page_parser.find_all('tr')
             for each_subject in term_table:
@@ -28,7 +29,7 @@ with open('UCI files.csv', mode = 'w', newline = '') as csvfile:
                 subject_split = None
                 if each_subject_link is not None:
                     subject_split = each_subject_link.text.split()
-                print(subject_split)
+                #print(subject_split)
                 if each_subject_link is not None and subject_split != list() and (subject_split[0] == 'Computer' or subject_split[0] == 'CSE'):
                     cs_year = term_page+'/' + each_subject_link.get('href')
                     cs_parser = BeautifulSoup(get(cs_year).text, 'lxml')
@@ -37,6 +38,14 @@ with open('UCI files.csv', mode = 'w', newline = '') as csvfile:
                         enrollment_info = each_class.find_all('td', {'align':'right'})
                         class_type = each_class.find_all('td')
                         if enrollment_info != list() and len(class_type) > 1 and class_type[1].text == 'Lec':
+                            professor_block = each_class.find_all('td', {'bgcolor':'#D5E5FF'})
+                            professor = None
+                            class_num = None
+                            if professor_block != list():
+                                professor = professor_block[2].text
+                                class_num = professor_block[0].text
+                                #print(class_num)
+                            #print(professor_block)
                             capacity_enrolled = enrollment_info[1].text
                             split = capacity_enrolled.split('/')
                             if len(split) == 2:
@@ -44,7 +53,8 @@ with open('UCI files.csv', mode = 'w', newline = '') as csvfile:
                                     'num_people_enrolled':split[0],
                                     'total_class_size':split[1],
                                     'term': term_year,
-                                    'class_number': None
+                                    'class_number': class_num,
+                                    'professor': professor
                                 }
                                 writer.writerow(class_info)
                             else:
@@ -52,6 +62,7 @@ with open('UCI files.csv', mode = 'w', newline = '') as csvfile:
                                     'num_people_enrolled':enrollment_info[1].text,
                                     'total_class_size':enrollment_info[0].text,
                                     'term':term_year,
-                                    'class_number':None
+                                    'class_number': class_num,
+                                    'professor': professor
                                 }
                                 writer.writerow(class_info)
